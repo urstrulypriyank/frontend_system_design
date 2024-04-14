dbcf32721ca7f333ce3bb97487fa6d3db87d66cf
priyank rai <type2email@gmail.com>
2024-04-14 12:47:33 +0530

    wrote request headers

--- CN/RestApi/index.js
+++ CN/RestApi/index.js
@@ -3,6 +3,53 @@ import express from "express"
 const app = express();
 const PORT = 5111;
 
+const DATA = [
+    {
+        titile: "Work One",
+        id: 0,
+        isCompleted: false,
+
+    }, {
+        titile: "Work 2",
+        id: 1,
+        isCompleted: false,
+
+    },
+     {
+        titile: "Work 3",
+        id: 2,
+        isCompleted: false,
+
+    },
+    {
+        titile: "Work 4",
+        id: 3,
+        isCompleted: false,
+
+    }
+]
+
+// create 
+
+
+// Read
+app.get('/todos',(req,res)=>{
+    res.status(200).send(DATA);
+})
+
+
+// Update
+
+
+
+// Delete
+
+
+
+
+
+
+
 
 app.all('/*', (req, res) => {
     console.log(req.url)
@@ -11,5 +58,5 @@ app.all('/*', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log("Server is running at https://localhost:" + PORT);
+    console.log("Server is running at http://localhost:" + PORT);
 })
